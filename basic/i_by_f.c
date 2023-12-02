a415688cb5f53f70ce1ca6acbce5c4c6609d8e90
mankumsaini <72141736+mankumsaini@users.noreply.github.com>
2023-12-02 21:06:19 +0530

    Add files via upload

---
+++ basic/i_by_f.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+	int i;
+	float f,ans;
+
+	printf("entre an int: ");
+	scanf("%d",&i);
+
+	printf("entre an float: ");
+	scanf("%f",&f);
+
+	ans=i/f;
+
+	printf("ans=%f",ans);
+}
